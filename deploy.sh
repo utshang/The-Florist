92fe2277355409fb789a0e5b46922c2093929ea8
Ashley Shang <utshang216@gmail.com>
2022-10-31 15:15:43 +0800

    change repo name

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/utshang/vue-final.git master:gh-pages
+git push -f https://github.com/utshang/The-Florist.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-final/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/The-Florist/" : "/",
 };
