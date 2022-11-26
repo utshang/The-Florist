52380f55d19994f959e1466e57361b480f828bd8
Ashley Shang <utshang216@gmail.com>
2022-11-26 19:49:45 +0800

    adjust variable name

--- src/mixins/FavoriteMixin.js
+++ src/mixins/FavoriteMixin.js
@@ -4,6 +4,7 @@ export default {
     return {
       //從 localStorage 取出資料
       //給預設值[]
+      fixFav: false,
       favorite: JSON.parse(localStorage.getItem("favorite")) || [],
     };
   },
@@ -11,18 +12,18 @@ export default {
     toggleFav(id) {
       //查 favorite 中有沒有這一個 id
       //findIndex() 方法返回陣列中滿足提供的測試函式的第一個元素的索引。若沒有找到對應元素則返回-1。
-      const favIDd = this.favorite.findIndex((item) => item === id);
+      const favID = this.favorite.findIndex((item) => item === id);
 
       //如果沒有這一個 id，push 進去 favorite 裡
-      if (favIDd === -1) {
+      if (favID === -1) {
         this.favorite.push(id);
         this.emitter.emit("push-message", {
           style: "success",
           title: "已經加入最愛囉！",
         });
-        //如果有這一個 id，從 favorite 中將 favIDd 刪掉
+        //如果有這一個 id，從 favorite 中將 favID 刪掉
       } else {
-        this.favorite.splice(favIDd, 1);
+        this.favorite.splice(favID, 1);
         this.emitter.emit("push-message", {
           style: "danger",
           title: "已經移除最愛囉！",
